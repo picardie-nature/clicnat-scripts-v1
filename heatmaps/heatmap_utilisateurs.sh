ae3abaef954290196bffee73cee2d0503a9bf599
jbh <jb@etoele.com>
2018-02-15 15:17:52 +0100

    Update heatmap scripts to fit 2017 obs creation date

--- heatmaps/heatmap_utilisateurs.sh
+++ heatmaps/heatmap_utilisateurs.sh
@@ -11,7 +11,7 @@ fi
 export PYTHONPATH=$PWD/osmviz/src/
 
 if [[ ! -f upoints ]]; then
-        echo "copy (select st_y(utilisateur.the_geom) as y, st_x(utilisateur.the_geom) as x from utilisateur,espace_departement where st_intersects(utilisateur.the_geom,espace_departement.the_geom) and utilisateur.the_geom is not null and espace_departement.reference in ('02','60','80')) to stdout;"|psql -h sgc clicnat pn > upoints
+        echo "copy (select st_y(utilisateur.the_geom) as y, st_x(utilisateur.the_geom) as x from utilisateur,espace_departement where st_intersects(utilisateur.the_geom,espace_departement.the_geom) and utilisateur.the_geom is not null and espace_departement.reference in ('02','60','80') and utilisateur.id_utilisateur in (select oo.id_utilisateur from observations o, observations_observateurs oo WHERE o.id_observation=oo.id_observation and extract('year' from date_observation) =  2017)) to stdout;"|psql -h sgc clicnat pn > upoints
 fi
 
 fsortie=$(date +"observateurs_%Y%m%d.png")
@@ -27,8 +27,9 @@ python heatmap/heatmap.py \
         --zoom 9 \
         -o $fsortie\
         -d 0.6 \
-	-r 14\
+	-r 18\
         -m "10088ff10" \
         -M "000ffffff" \
-        --osm_base "http://gpic.web-fr.org/mapproxy/wmts/basemaps_google/GLOBAL_MERCATOR/"
+        --osm
+        #--osm_base "http://gpic.web-fr.org/mapproxy/wmts/basemaps_google/GLOBAL_MERCATOR/"
 
